c6f3557de1eaf033fec315c3704ec551bc35a665
Tumisng <48886506+Tumisng@users.noreply.github.com>
2023-10-03 22:12:15 +0200

    added the modulo and subtraction function

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 name = input ("what is your name? ")
 
-a = ("what is your x?")
-b = ("what is your y?")
+a = ("what is your a?")
+b = ("what is your b?")
 
 c = a + b
 
-d = ("what is your input for a, it should be greater than 10?")
-e = ("what is your input for b, it should be less than 10?")
+d = ("what is your input for d, it should be greater than 10?")
+e = ("what is your input for e, it should be less than 10?")
 
 f = d - e
 
@@ -15,9 +15,14 @@ h = ("what is your input for h?")
 
 i = g * h
 
+x = ("what is your input for x, it should be greater than 10?")
+y = ("what is your input for y, it should be less than 10?")
+
+z = x % y
 
 print("hello,", name)
 print("your answer for addtion is", c)
 print("your answer for subtraction is", f)
-print("your answer for subtraction is", i)
+print("your answer for multiplication is", i)
+print("your answer for modulo is", z)
 
