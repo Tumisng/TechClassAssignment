234898bcce579c80004e25b843330fd3596496e9
Tumisng <48886506+Tumisng@users.noreply.github.com>
2023-10-03 21:56:34 +0200

    added a subtraction and multiplication function

--- hello.py
+++ hello.py
@@ -1,12 +1,23 @@
 name = input ("what is your name? ")
 
-x = ("what is your x?")
-y = ("what is your y?")
+a = ("what is your x?")
+b = ("what is your y?")
 
-z = x + y
+c = a + b
 
-z = x - y
+d = ("what is your input for a, it should be greater than 10?")
+e = ("what is your input for b, it should be less than 10?")
+
+f = d - e
+
+g = ("what is your input for g?")
+h = ("what is your input for h?")
+
+i = g * h
+
 
 print("hello,", name)
-print("your answer is", z)
+print("your answer for addtion is", c)
+print("your answer for subtraction is", f)
+print("your answer for subtraction is", i)
 
